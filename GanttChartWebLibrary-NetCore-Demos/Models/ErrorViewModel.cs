cba6cbfa3a01988680040738131ccbc240af3296
DlhSoft <Contact@DlhSoft.com>
2020-09-23 15:59:43 +0300

    Initial commit

---
+++ GanttChartWebLibrary-NetCore-Demos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GanttChartWebLibrary_NetCore_Demos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
